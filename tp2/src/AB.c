199b7072e65ded5ae4267f77ab12088d46ee813b
Anais Aoucher <45401649+Anais1804@users.noreply.github.com>
2023-03-14 10:56:03 +0100

    tp2

---
+++ tp2/src/AB.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../include/AB.h"
+
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+
+
+Arbre alloue_noeud(int val){
+    Noeud* nouveau = (Noeud*) malloc(sizeof(Noeud));
+
+    if(!nouveau)
+        return NULL;
+    nouveau->val = val;
+    nouveau->fg = NULL;
+    nouveau->fd = NULL;
+
+    return nouveau; 
+}
+
+
+void affiche_arbre(Arbre a){
+    if(!a) return;
+    printf("%d\n", a->val);
+
+    affiche_arbre(a->fg);
+    affiche_arbre(a->fd);
+}
+
+
+int hauteur(Arbre a){
+    int hg, hd;
+
+    if (!a)
+        return -1;
+    
+    hg = hauteur(a->fg);
+    hd = hauteur(a->fd);
+
+    return 1 + MAX(hg, hd);
+}
+
+int nb_noeud(Arbre a){
+    if(!a)
+        return 0;
+    return 1 + nb_noeud(a->fg)+ nb_noeud(a->fd);
+}
+
+
+int nb_noeud_interne(Arbre a){
+    if(!a) 
+        return 0;
+    if(((!a->fg) && (!a->fd)))
+        return 0;
+    return 1 + nb_noeud_interne(a->fg) + nb_noeud_interne(a->fd);
+}
+
+
+int nb_feuille(Arbre a){
+    if(!a)
+		return 0;
+	if(!a->fg && !a->fg)
+			return 1;
+	return nb_feuille(a->fg)+nb_feuille(a->fd);
+}
+
+
+int nb_noeud_deux_fils(Arbre a){
+    if(!a) 
+        return 0;
+    if(a->fg && a->fd)
+        return 1 + nb_noeud_deux_fils(a->fg) + nb_noeud_deux_fils(a->fd);;
+    return nb_noeud_deux_fils(a->fg) + nb_noeud_deux_fils(a->fd);
+
+}
+
+
+int est_strictement_binaire(Arbre a){
+    if((a->fg == NULL && a->fd != NULL) || (a->fg != NULL && a->fd == NULL))
+        return 0;
+    return 1;
+}
+
+
+Arbre construit_arbre(FILE *fichier) {
+    int val;
+    Arbre a;
+
+    fscanf(fichier, "%d", &val);
+
+    if(val == 0){
+        return NULL;
+    }
+    
+    if(!(a = alloue_noeud(val)))
+        return NULL;
+    
+    a->fg = construit_arbre(fichier);
+    a->fd = construit_arbre(fichier);
+
+    return a;
+}
+
+
+void libere(Arbre *a){
+    if(!(*a))
+        return;
+    libere(&(*a)->fg);
+    libere(&(*a)->fd);
+
+    free((*a));
+    (*a) = NULL;
+} 
---
+++ tp2/src/tp2.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../include/AB.h"
+
+int main(int argc, char** argv){
+    
+    /*Arbre a = alloue_noeud(1);
+    a->fg = alloue_noeud(2);
+    a->fd = alloue_noeud(5);
+
+    a->fg->fg = alloue_noeud(3);
+    a->fg->fd = alloue_noeud(4);
+    
+    a->fd->fd = alloue_noeud(6);
+    a->fd->fg = alloue_noeud(7); */
+
+
+    FILE* f = fopen(argv[1], "r");
+
+    if(!f){
+        printf("Aucun fichier passé en argument");
+        exit(0);
+    }
+    
+    Arbre a;
+    a = construit_arbre(f); 
+
+
+    printf("hauteur = %d\n", hauteur(a));
+    printf("nb noeuds = %d\n", nb_noeud(a));
+    printf("nb noeuds internes = %d\n", nb_noeud_interne(a));
+    printf("nb feuilles = %d\n", nb_feuille(a));
+    printf("nb deux fils = %d\n", nb_noeud_deux_fils(a));
+    printf("strictement binaire ? %d\n", est_strictement_binaire(a));
+
+    fclose(f);
+    libere(&a);
+    return 0;
+}
